a62997773127f87b2dc428f6f3dc143430815625
Kevin Yuan <phoenixlwp@gmail.com>
2025-10-14 13:06:16 +0800

    added two projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,6 +9,20 @@ export const projects = [
     githubUrl: "https://github.com/phoenixlwpapix/cicada-english",
   },
   {
+    title: "Math Toolkit",
+    description: "帮你更好理解数学、物理、化学知识。",
+    imageUrl: "https://placehold.co/600x400/71A9F7/FFFFFF?text=Math%20Toolkit",
+    demoUrl: "https://math-toolkit.studioyyh.tech/",
+    githubUrl: "https://github.com/phoenixlwpapix/math-toolkit",
+  },
+  {
+    title: "PRD Genie",
+    description: "帮你更好编写项目PRD文件并建议技术栈。",
+    imageUrl: "https://placehold.co/600x400/C0E8F9/FFFFFF?text=PRD%20Genie",
+    demoUrl: "https://prd-genie.studioyyh.tech/",
+    githubUrl: "https://github.com/phoenixlwpapix/prd-genie",
+  },
+  {
     title: "Chez Liliane",
     description: "一个烘焙爱好者的个人主页。",
     imageUrl: "https://placehold.co/600x400/ffc0cb/FFFFFF?text=Chez%20Liliane",
@@ -16,10 +30,11 @@ export const projects = [
     githubUrl: "https://github.com/phoenixlwpapix/phoenixlwpapix.github.io",
   },
   {
-    title: "Math Toolkit",
-    description: "帮你更好理解数学、物理、化学知识。",
-    imageUrl: "https://placehold.co/600x400/71A9F7/FFFFFF?text=Math%20Toolkit",
-    demoUrl: "https://math-toolkit.studioyyh.tech/",
-    githubUrl: "https://github.com/phoenixlwpapix/math-toolkit",
+    title: "Cosmic Flow",
+    description:
+      "一个动态占星与月相追踪器，提供实时宇宙能量报告、个性化能量指引和行星影响的运用练习。",
+    imageUrl: "https://placehold.co/600x400/96E6B3/FFFFFF?text=Cosmic%20Flow",
+    demoUrl: "https://cosmic-flow.studioyyh.tech/",
+    githubUrl: "https://github.com/phoenixlwpapix/cosmic-flow",
   },
 ];

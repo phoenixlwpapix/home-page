4a0f3cfd40ee089f2382891523e6e776c740948f
Kevin Yuan <phoenixlwp@gmail.com>
2025-10-17 11:13:01 +0800

    UI change

--- src/data/projects.js
+++ src/data/projects.js
@@ -25,22 +25,21 @@ export const projects = [
   {
     title: "Chez Liliane",
     description: "一个烘焙爱好者的个人主页。",
-    imageUrl: "https://placehold.co/600x400/ffc0cb/FFFFFF?text=Chez%20Liliane",
+    imageUrl: "https://placehold.co/600x400/f07491/FFFFFF?text=Chez%20Liliane",
     demoUrl: "https://bakery.studioyyh.tech/",
     githubUrl: "https://github.com/phoenixlwpapix/phoenixlwpapix.github.io",
   },
   {
     title: "Weather Wizard",
-    description: "A magical weather forecasting app with AI predictions.",
+    description: "一款神奇的天气预报应用，配备AI预测功能。",
     imageUrl:
-      "https://placehold.co/600x400/87CEEB/FFFFFF?text=Weather%20Wizard",
+      "https://placehold.co/600x400/0BBE99/FFFFFF?text=Weather%20Wizard",
     demoUrl: "https://weather-wizard.studioyyh.tech/",
     githubUrl: "https://github.com/phoenixlwpapix/weather-wizard",
   },
   {
     title: "Recipe Rover",
-    description:
-      "Explore recipes from around the world with smart ingredient matching.",
+    description: "通过智能配料匹配，探索世界各地的美食食谱。",
     imageUrl: "https://placehold.co/600x400/FFA500/FFFFFF?text=Recipe%20Rover",
     demoUrl: "https://recipe-rover.studioyyh.tech/",
     githubUrl: "https://github.com/phoenixlwpapix/recipe-rover",
